70945a4330a0f7d7e18e1761d4b886f381d570e9
remagine <remagine@naver.com>
2023-04-18 00:10:18 +0900

    0417

---
+++ src/main/java/playground/_230415/Main.java
@@ -0,0 +1,24 @@
+package playground._230415;
+public class Main {
+    public static void main(String[] args ){
+        String data = "hello world";
+        byte[] bytes = data.getBytes("utf-8");
+
+        InputStream is = new ByteArrayInputStream(bytes,0,bytres.length);
+        BufferedInputStream bis = new Bis(is, 8192);
+        InputStreamReader isr = new ISR(bis, "utf-8");
+
+        char[] cBuffer = new char[1024];
+        StringBuild sb = new StirngBuild();
+        while(true){
+            int len = isr.read(cBuffer);
+            if(len == -1 ){
+                break;
+            }
+            sb.append(cBuffer, 0 , len);
+        }
+        print(sb);
+
+    }
+
+}
